69226a0c7ba008a55901fecee62763389f17bb49
damirturgunabev10 <damirturgunbaev755@gmail.com>
2026-05-04 17:57:06 +0500

    testing again

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { SplitLayout } from "../components/layout/SplitLayout";
-import { GlassCard } from "../components/ui/GlassCard";
+import  GlassCard from "../components/ui/GlassCard";
 
 import ReadingContent from "../features/reading/ReadingContent";
 import ReadingTask from "../features/reading/ReadingTask";
