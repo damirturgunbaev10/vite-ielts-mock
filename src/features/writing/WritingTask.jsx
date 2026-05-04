7d36f39f45b0b1528c4091bba4f830506e643c1b
damirturgunabev10 <damirturgunbaev755@gmail.com>
2026-05-04 17:59:14 +0500

    test

--- src/features/writing/WritingTask.jsx
+++ src/features/writing/WritingTask.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GlassCard } from "../../components/ui/GlassCard";
+import GlassCard from "../../components/ui/GlassCard";
 
 const WritingTask = ({ data }) => {
   if (!data) return null;
